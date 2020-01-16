70dd6f26401e6e833f73e8a4c77265c5cda15913
Yu <13821221326@163.com>
2020-01-16 14:49:16 +0800

    Update multiselect.go

--- multiselect.go
+++ multiselect.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/AlecAivazis/survey/v2/core"
-	"github.com/AlecAivazis/survey/v2/terminal"
+	"github.com/xcode03/survey/v2/core"
+	"github.com/xcode03/survey/v2/terminal"
 )
 
 /*
